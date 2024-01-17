e89018907e96a96e5e28ce9b2745e62e934d38fd
Fishel <nfishel@hse.k12.in.us>
2024-01-17 11:18:05 -0500

    Create stringNotes.py

---
+++ stringNotes.py
@@ -0,0 +1,6 @@
+first = "        LaNCe              "
+last  = "   jONeS      "
+full  = "LAncE jONes"
+
+
+# use the handout to understand the different string methods that we commonly use in Python
