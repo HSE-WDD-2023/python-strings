33774b710f88aaf29b3bff32c2ab480e8f097e3c
Fishel <nfishel@hse.k12.in.us>
2024-01-18 20:40:21 -0500

    Update stringNotes.py

--- stringNotes.py
+++ stringNotes.py
@@ -1,6 +1,10 @@
+from os import system
+system("clear")
+
 first = "        LaNCe              "
 last  = "   jONeS      "
 full  = "LAncE jONes"
 
 
 # use the handout to understand the different string methods that we commonly use in Python
+print("|" + first + "|")
