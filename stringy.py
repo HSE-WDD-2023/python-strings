b2d4c2d216d0af0d5bfd64a07f5e89d69f11dbb3
Fishel <nfishel@hse.k12.in.us>
2024-01-19 07:59:07 -0500

    Update stringy.py

---
+++ stringy.py
@@ -0,0 +1,7 @@
+from os import system
+system("clear")
+
+hse = "        HaMiltOn sOuTHeaStErn ROyaLs              "
+
+# follow along in class to understand the different string methods that we commonly use in Python
+
